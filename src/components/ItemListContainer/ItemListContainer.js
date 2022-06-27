33b46a2c4058a0492f6efe73539a217011492e54
BuildTools <unconfigured@null.spigotmc.org>
2022-06-27 19:35:07 -0300

    cards

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './ItemListContainer.css'
-
-const ItemListContainer = ({greeting}) => {
+import  ItemCount  from '../ItemCount/ItemCount'
+import img1 from '../../assets/Img/img1.jpg'
+import img2 from '../../assets/Img/img2.jpg'
+import img3 from '../../assets/Img/img3.jpg'
+const ItemListContainer = () => {
   return (
-    <div> {greeting} </div>
+    <div className='container'> 
+      <ItemCount url = {img1} stock = {10} producto = {'Buzo Llamas'} />
+      <ItemCount url = {img2} stock = {10} producto = {'Camperon Azul'}/>
+      <ItemCount url = {img3} stock = {10} producto = {'Gorro/Bufanda'}/>
+    </div>
   )
 }
 
